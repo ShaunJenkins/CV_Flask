0f6b0fa716d0f257883c5c0ed37ceb4829df4f86
Djeradi Imedine <69174572+ShaunJenkins@users.noreply.github.com>
2024-02-08 12:25:55 +0100

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -1,1 +1,12 @@
-###test
+import sqlite3
+
+connection = sqlite3.connect('database.db')
+
+with open('schema.sql') as f:
+    connection.executescript(f.read())
+
+cur = connection.cursor()
+
+
+connection.commit()
+connection.close()
